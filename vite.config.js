4b1fc00b9d71a2622437c625c45672b26dc67e0a
terse <okekechidera992@gmail.com>
2025-05-06 14:10:35 -0700

    gfffff

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port:  5173,
+    allowedHosts: ['portfolio-2msx.onrender.com'],
   },
   preview: {
     host: '0.0.0.0',
-    port:  4173, 
+    port:  4173,
   }
 })
